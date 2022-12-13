0f16b598f39b9806d09a948bb02fc271f56b704a
noceive <nocivescience@gmail.com>
2022-12-13 03:04:23 -0300

    mi primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// class Muestra{
+//     constructor(number){
+//         this.number=number
+//     }
+//     powNumber(){
+//         return Math.pow(this.number,3)
+//     }
+// }
+// const muestra= new Muestra(4)
+function Ejecutor(){
+    let myClass='bg-warning text-center'
+    const palabra=<div className={myClass}>Estoy probando React</div>
+    return palabra
+}
+const root= ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div>
+        <Ejecutor/>
+    </div>
+)
